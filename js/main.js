68f0663fe14966e87b99b017ce43658319772df8
Antônio Bruno Gonçalves de Sousa <brunogs1995@gmail.com>
2016-09-02 18:21:53 -0300

    Criando plugin planetsBuilder.

--- js/main.js
+++ js/main.js
@@ -1,65 +1,78 @@
 
-$(document).ready(function() {
+jQuery.fn.planetsBuilder = function() {
 
-	var json = $.getJSON("js/data-planets.json", function(data) {
-
-		var aux = null;
-
-		$.each(data['planets'], function(key, value){
-			spacePopulation(key, value);
-
-			if(key == 'plutao') return false;		
-		});
-
-		$('.blockSolarSys li').click(function(e) {
-
-			var keyClass = $(this).attr('class');			
+	this.init = function() {
 			
+		that = this;	
+
+		var json = $.getJSON("js/data-planets.json", function(data) {
+
+			var aux = null;
+
 			$.each(data['planets'], function(key, value){
+				that.spacePopulation(key, value);
 
-				if(key == keyClass && key != 'sol') {
+				if(key == 'plutao') return false;		
+			});
 
-					aux = key;
+			$('.blockSolarSys li').click(function(e) {
 
-					showInformation(key, value);
+				var keyClass = $(this).attr('class');			
+					
+				$.each(data['planets'], function(key, value){
 
-					return false;
-				}	
+					if(key == keyClass && key != 'sol') {
+
+						aux = key;
+
+						that.showInformation(key, value);
+
+						return false;
+					}	
+
+				});
 
 			});
 
 		});
 
-	});
+	}
 
-	function showInformation(chave, valor) {
+	this.spacePopulation = function(key, value) {
+		$('.blockSolarSys').append($('<li/>').addClass(''+ key +'').append($('<img/>').attr('src', value.urlImg)));
+	}
+
+	this.showInformation = function(key, value) {
 		$('.descricoes').find('p').remove();
 		$('.bullets').find('li').remove();
 		
-		$('.img-planet').attr('src', valor.urlImg2);	
-		$('.titulo').text(chave);
+		$('.img-planet').attr('src', value.urlImg2);	
+		$('.titulo').text(key);
 
-		$('.descricoes').append($('<p/>').addClass('texto').text(valor.infos['str1']));
+		$('.descricoes').append($('<p/>').addClass('texto').text(value.infos['str1']));
 
-		for(var k = 0; k < valor.bullets; k++) {
-			$('.bullets').append($('<li/>').addClass('bull-item').text(''+(k+1)+''));
+		for(var k = 0; k < value.bullets; k++) {
+			$('.bullets').append($('<li/>').addClass('bull-item').text(''+( k+1 )+''));
 		}
 
 		$('.bullets li:first-child').addClass('active');
 
-		bulletsItemClick(chave, valor);
+		this.bulletsItemClick(value);	
 	}
 
-	function spacePopulation(chave, valor) {
-		$('.blockSolarSys').append($('<li/>').addClass(''+chave+'').append($('<img/>').attr('src', valor.urlImg)));
-	}
-
-	function bulletsItemClick(chave, valor) {
+	this.bulletsItemClick = function(value) {
 		$('.bull-item').click(function() {
-			$('.bull-item').removeClass('active');
-			$('.descricoes p').text(valor.infos['str'+ $(this).text() +'']);			
+		$('.bull-item').removeClass('active');
+
+		$('.descricoes p').text(value.infos['str'+ $(this).text() +'']);			
 			$(this).addClass('active');
 		});
 	}
 
-});
+	this.init();
+
+}
+
+jQuery(document).ready(function(){
+	jQuery('body').planetsBuilder();
+})
